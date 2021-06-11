5e2febdbf6bfe2e312bdb43709e5bb81a6dd04e2
jannik <giese.jannik@web.de>
2021-06-11 17:26:08 +0200

    :sparkles: Add Euro Button
    without function atm

---
+++ src/app/components/EuroButton/EuroButton.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import EuroButton from "./EuroButton";
+
+export default {
+  title: "Component/EuroButton",
+  component: EuroButton,
+};
+
+export const Euro = (): JSX.Element => <EuroButton />;
