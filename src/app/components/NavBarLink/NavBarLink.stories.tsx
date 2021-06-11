5723be9b78155fff3b86164715f7d52f7ded458e
jannik <giese.jannik@web.de>
2021-06-11 15:23:03 +0200

    :sparkles: Add the Copmass/Map link to the NavBar

--- src/app/components/NavBarLink/NavBarLink.stories.tsx
+++ src/app/components/NavBarLink/NavBarLink.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BurgerMenuIcon from "../Icons/BurgerMenuIcon";
+import CompassIcon from "../Icons/CompassIcon";
 import NavBarLink from "./NavBarLink";
 
 export default {
@@ -8,4 +9,7 @@ export default {
 };
 export const BurgerMenu = (): JSX.Element => (
   <NavBarLink icon={<BurgerMenuIcon />} link="#" />
+);
+export const Compass = (): JSX.Element => (
+  <NavBarLink icon={<CompassIcon />} link="#" />
 );
