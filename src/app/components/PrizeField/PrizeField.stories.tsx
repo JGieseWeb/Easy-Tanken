33294940efddbf17d3f39aae3b54cdf8036f6d1c
jannik <giese.jannik@web.de>
2021-06-16 15:02:42 +0200

    ✨Add Prize field Component with first PrizeField

---
+++ src/app/components/PrizeField/PrizeField.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PrizeField from "./PrizeField";
+
+export default {
+  title: "Component/PrizeField",
+  component: PrizeField,
+};
+export const FirstPrizeField = (): JSX.Element => <PrizeField />;
---
+++ src/app/components/PrizeField/PrizeField.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./PrizeField.module.css";
+
+export default function PrizeField(): JSX.Element {
+  return <div className={styles.prizeField}>1.60</div>;
+}
