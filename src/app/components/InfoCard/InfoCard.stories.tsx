6c0f030d51a22b1aff9a5625e5349aac6a2fdeb8
jannik <giese.jannik@web.de>
2021-06-17 14:46:19 +0200

    ✨create InfoCard component

---
+++ src/app/components/InfoCard/InfoCard.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InfoCard from "./InfoCard";
+
+export default {
+  title: "Component/InfoCard",
+  component: InfoCard,
+};
+export const InfoCardvar = (): JSX.Element => <InfoCard />;
