13135cf153c779a369a4cda4def1ce1fd69ea7ba
jannik <giese.jannik@web.de>
2021-06-18 15:37:52 +0200

    🎨combine variables that was equal

--- src/app/components/PrizeField/PrizeField.tsx
+++ src/app/components/PrizeField/PrizeField.tsx
@@ -8,8 +8,10 @@ export type PrizeFieldProps = {
 export default function PrizeField({ variant }: PrizeFieldProps): JSX.Element {
   return (
     <div className={styles.container}>
-      <div className={styles[`${variant}__prize`]}>1.60</div>
-      <div className={styles[`${variant}__bar`]}></div>
+      <div className={`${styles.prize} ${styles[`${variant}__prize`]}`}>
+        1.60
+      </div>
+      <div className={`${styles.bar} ${styles[`${variant}__bar`]}`}></div>
     </div>
   );
 }
