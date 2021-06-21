4ecaf4e7f751278be666f768362d8abf786f096a
jannik <giese.jannik@web.de>
2021-06-21 11:25:49 +0200

    :art: Make the prize more response

--- src/app/components/PrizeField/PrizeField.tsx
+++ src/app/components/PrizeField/PrizeField.tsx
@@ -3,13 +3,17 @@ import styles from "./PrizeField.module.css";
 
 export type PrizeFieldProps = {
   variant: "primary" | "secondary";
+  prize: number;
 };
 
-export default function PrizeField({ variant }: PrizeFieldProps): JSX.Element {
+export default function PrizeField({
+  variant,
+  prize,
+}: PrizeFieldProps): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={`${styles.prize} ${styles[`${variant}__prize`]}`}>
-        1.60
+        {prize}
       </div>
       <div className={`${styles.bar} ${styles[`${variant}__bar`]}`}></div>
     </div>
