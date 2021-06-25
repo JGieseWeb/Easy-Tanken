455716a3be7ca916539ebcdabfba70268bf7f8c5
jannik <giese.jannik@web.de>
2021-06-25 11:29:49 +0200

    ✨Implement to SB

---
+++ src/app/components/Spritcard/SpritCard.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import SpritCard from "./SpritCard";
+
+export default {
+  title: "Component/SpritCard",
+  component: SpritCard,
+};
+
+export const SpritSort = (): JSX.Element => <SpritCard />;
