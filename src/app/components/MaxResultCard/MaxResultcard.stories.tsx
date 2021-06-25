edb5c16ad7c0ae3e49c06f0a8862a9b7785e9799
jannik <giese.jannik@web.de>
2021-06-25 15:53:44 +0200

    ✨Implement MaxResultCard into sb

---
+++ src/app/components/MaxResultCard/MaxResultcard.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import MaxResultCard from "./MaxResultCard";
+
+export default {
+  title: "Component/MaxResultCard",
+  component: MaxResultCard,
+};
+
+export const MaxResult = (): JSX.Element => <MaxResultCard />;
