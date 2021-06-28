e164d0ef54802faadde01f20c7ea157070261104
jannik <giese.jannik@web.de>
2021-06-28 14:05:43 +0200

    ✨Implment Radius ito sB

---
+++ src/app/components/Radius/Radius.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import RadiusCard from "./Radius";
+
+export default {
+  title: "Component/RadiusCard",
+  component: RadiusCard,
+};
+
+export const Radius = (): JSX.Element => <RadiusCard />;
