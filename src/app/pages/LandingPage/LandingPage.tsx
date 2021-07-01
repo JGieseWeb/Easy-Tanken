665f65e4e5c411d624e4134005e23a7310b56c0a
jannik <giese.jannik@web.de>
2021-07-01 17:47:18 +0200

    ✨fetch&map on LandingPage

--- src/app/pages/LandingPage/LandingPage.tsx
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import TankCard from "../../components/TankCard/TankCard";
 import styles from "./LandingPage.module.css";
+import { Station } from "../../../Server/types";
 
 function LandingPage(): JSX.Element {
+  const { search } = useLocation();
+
+  const [stations, setStations] = useState<Station[]>([]);
+
+  useEffect(() => {
+    async function fetchStations() {
+      const response = await fetch(`/api/list${search}`);
+      const stations = await response.json();
+      setStations(stations);
+    }
+    fetchStations();
+  }, []);
+
   return (
     <div className={styles.container}>
       <header>
         <NavBar />
       </header>
       <main className={styles.main}>
-        <TankCard tankName="Aral" variant="primary" />
-        <TankCard tankName="Esso" variant="secondary" />
+        {stations.map((station, index) => (
+          <TankCard
+            key={station.id}
+            tankName={station.name}
+            variant={index === 0 ? "primary" : "secondary"}
+          />
+        ))}
       </main>
     </div>
   );
