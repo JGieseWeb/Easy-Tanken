de37b8722c9cabf5dbee479100cadb53b4da2e77
jannik <giese.jannik@web.de>
2021-07-01 17:26:35 +0200

    ✨Add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,49 +1,21 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <Router>
-      <div className={styles.App}>
-        <header className={styles["App-header"]}>
-          <img src={logo} className={styles["App-logo"]} alt="logo" />
-          <p>Hello Vite + React!</p>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles["App-link"]}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {" | "}
-            <a
-              className={styles["App-link"]}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-          <Switch>
-            <Route></Route>
-          </Switch>
-        </header>
-      </div>
+      <Switch>
+        <main>
+          <Route path="/settings">
+            <SettingsPage />
+          </Route>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </main>
+      </Switch>
     </Router>
   );
 }
